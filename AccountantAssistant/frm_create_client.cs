7958bbb2cafe17b34a4526eacf3852ac530b8cd1
Nico Brandtner <n.brandtner@hak-kitz.ac.at>
2021-11-17 11:03:57 +0100

    17.11 Create Client

---
+++ AccountantAssistant/frm_create_client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountantAssistant
+{
+    public partial class frm_create_client : Form
+    {
+        public frm_create_client()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            frm_main frm_Main = new frm_main();
+            this.Close();
+          
+        }
+    }
+}
